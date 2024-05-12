c60c29c57cf108a981fa9aaae391f31497d0a561
KennethDom <102487889+KennethDom@users.noreply.github.com>
2024-05-11 19:50:12 -0600

    Examples 2

---
+++ Examples 2/ejemploselect.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="datos.php" method="GET">
+    <p>
+    <select name="cursos">		
+        <option value=""></option>		
+        <option value="php y mysqlx">php y mysql</option>		
+        <option value="framework codeigniter">framework codeigniter</option>		
+        <option value="wordpress">wordpress</option>		
+        <option value="ajax" selected>ajax</option>	
+    </select>	
+
+    </p>	
+    <input type="submit">
+</form>
+</body>
+</html>
